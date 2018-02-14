a853bbb05c569fe2192dde83685cfb846d415abb
chandulella <chandulella77@gmail.com>
2018-02-14 09:17:03 +0530

    use this

---
+++ project/words.js
@@ -0,0 +1,13 @@
+module.exports.obj={
+    details:{
+      name:{
+        first_name:'',
+        last_name:''
+      },
+      gender:'',
+      dateOfBirth:'',
+      email:'',
+      mobile:''
+    }
+
+ }
